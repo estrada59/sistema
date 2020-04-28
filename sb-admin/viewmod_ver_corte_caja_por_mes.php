bb574316fefd055a09b3abf08e13b2a461ed122a
enrique estrada <estrada59@gmail.com>
2020-04-28 16:33:43 -0500

    se actualizó el nav sesion simplificado

--- sb-admin/viewmod_ver_corte_caja_por_mes.php
+++ sb-admin/viewmod_ver_corte_caja_por_mes.php
@@ -18,16 +18,9 @@
     <div id="wrapper">
 
         <!-- Navigation -->
-       <?php  
-       		if($_SESSION['nivel_acceso'] == 3){
-                include "nav.php";  }
-
-            if($_SESSION['nivel_acceso'] == 1){
-                include "nav_priv.php"; }
-                
-            if($_SESSION['nivel_acceso'] == 2){
-                include "nav_contador.php"; }
-       ?>
+        <?php
+            include_once 'include/nav_session.php';
+        ?>
         <div id="page-wrapper">
 
             <div class="container-fluid">
